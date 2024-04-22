008dd63d4dbca6b0fc23875fbeb5919ea43ae620
NFHr <87050903+NFHr@users.noreply.github.com>
2024-04-22 17:30:16 +0800

    Add vim to Dockerfile and update build.sh script

--- Docker/utils/build.sh
+++ Docker/utils/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # serial
-git clone https://github.com/RoverRobotics-forks/serial-ros2.git ~/src/serial_ros2
-cd ~/src/serial_ros2 || exit 1
+git clone https://github.com/RoverRobotics-forks/serial-ros2.git /src/serial_ros2
+cd /src/serial_ros2 || exit 1
 mkdir build
 cd build || exit 1
 cmake .. -DBUILD_SHARED_LIBS=ON && make
 sudo make install
 
 # YDLiDar SDK
-git clone https://github.com/YDLIDAR/YDLidar-SDK.git ~/src/YDLidar-SDK
-cd ~/src/YDLidar-SDK || exit 1
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git /src/YDLidar-SDK
+cd /src/YDLidar-SDK || exit 1
 mkdir build
 cd build || exit 1
 cmake ..
@@ -18,8 +18,14 @@ make -j4
 sudo make install
 
 # YDLiDar ROS2 Driver
-git clone -b humble https://github.com/YDLIDAR/ydlidar_ros2_driver.git ~/src/ydlidar_ws/src/ydlidar_ros2_driver
+git clone -b humble https://github.com/YDLIDAR/ydlidar_ros2_driver.git /src/ydlidar_ws/src/ydlidar_ros2_driver
 
-cd ~/src/ydlidar_ws || exit 1
+cd /src/ydlidar_ws || exit 1
 colcon build --symlink-install
 echo "source ~/src/ydlidar_ws/install/setup.bash --extend" >>~/.bashrc
+
+# MPU6050
+git clone https://github.com/hiwad-aziz/ros2_mpu6050_driver.git /src/mpu6050_ws/src/
+cd /src/mpu6050_ws || exit 1
+colcon build --packages-select mpu6050driver
+echo "source ~/src/mpu6050_ws/install/setup.bash --extend" >>~/.bashrc
