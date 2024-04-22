08a56e81061fcaaf75c3f699295cc583d3b913a4
NFHr <87050903+NFHr@users.noreply.github.com>
2024-04-23 02:50:45 +0800

    Update Dockerfile to include packages

--- Docker/utils/build.sh
+++ Docker/utils/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # serial
-git clone https://github.com/RoverRobotics-forks/serial-ros2.git /src/serial_ros2
-cd /src/serial_ros2 || exit 1
+git clone https://github.com/RoverRobotics-forks/serial-ros2.git /build_ws/serial_ros2
+cd /build_ws/serial_ros2 || exit 1
 mkdir build
 cd build || exit 1
 cmake .. -DBUILD_SHARED_LIBS=ON && make
 sudo make install
 
 # YDLiDar SDK
-git clone https://github.com/YDLIDAR/YDLidar-SDK.git /src/YDLidar-SDK
-cd /src/YDLidar-SDK || exit 1
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git /build_ws/YDLidar-SDK
+cd /build_ws/YDLidar-SDK || exit 1
 mkdir build
 cd build || exit 1
 cmake ..
@@ -18,7 +18,7 @@ make -j4
 sudo make install
 
 # YDLiDar ROS2 Driver
-git clone -b humble https://github.com/YDLIDAR/ydlidar_ros2_driver.git /src/ydlidar_ws/src/ydlidar_ros2_driver
-cd /src/ydlidar_ws || exit 1
+git clone -b humble https://github.com/YDLIDAR/ydlidar_ros2_driver.git /build_ws/ydlidar_ws/src/ydlidar_ros2_driver
+cd /build_ws/ydlidar_ws || exit 1
 colcon build --symlink-install
-echo "source /src/ydlidar_ws/install/setup.bash --extend" >>~/.bashrc
+echo "source /build_ws/ydlidar_ws/install/setup.bash --extend" >> ~/.bashrc
