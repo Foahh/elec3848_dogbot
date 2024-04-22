8d5d9a5cb37ea69b0ddaff1cb81f08c9c2ce651d
chron1kle <saundershwu@gmail.com>
2024-04-22 16:14:41 +0800

    S

--- dogbot_server/dogbot_server/dogbot_server.py
+++ dogbot_server/dogbot_server/dogbot_server.py
@@ -147,7 +147,7 @@ def main(args=None):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("0.0.0.0", 8080))
-    server_socket.listen(5)
+    server_socket.listen(5)  # docker 上的端口映射是多少？8080
     node = ServerPublisherNode(server_socket)
     node.get_logger().info("Waiting for connection...")
 
