e2dda09bf179e22aeb2e405f635666b7ef222fcc
chron1kle <saundershwu@gmail.com>
2024-04-22 17:04:22 +0800

    S

--- dogbot_server/dogbot_server/dogbot_client.py
+++ dogbot_server/dogbot_server/dogbot_client.py
@@ -1,14 +1,23 @@
-import socket
+import socket, threading
 
-def client(msg) -> None:
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    server_address = ('0.0.0.0', 8080)
-    client_socket.connect(server_address)
-
-    while True:
-        client_socket.sendall(msg.encode())
-        data = client_socket.recv(1024).decode()
-
-    client_socket.close()
+class ClientSide:
+    def __init__(self) -> None:
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_address = ('0.0.0.0', 8080)
+        self.client_socket.connect(server_address)
+        return
+    
+    def send(self, msg) -> object:
+        self.client_socket.sendall(msg.encode())
+        data = self.client_socket.recv(1024).decode()
+        return data
+    
+    def sending(self, msg) -> None:
+        sending_thread = threading.Thread(target=self.send, args=(msg))
+        sending_thread.start()
+        return
+    
+    def shutdown(self) -> None:
+        self.client_socket.close()
+        return
