72f3d0dcd06ebcf23c88057b166b0eb36d7011db
chron1kle <saundershwu@gmail.com>
2024-04-23 14:49:31 +0800

    S

--- serv.sh
+++ serv.sh
@@ -6,6 +6,7 @@
 # ./attach.sh
 source install/setup.bash
 colcon build
-ros2 run dogbot_server dogbot_server
+# ros2 run dogbot_server dogbot_server
+ros2 launch dogbot_bringup dogbot_launch.py
 # ros2 run 
 
