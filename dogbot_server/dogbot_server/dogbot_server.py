bf6580ecc9182ed6869e211671caa1ef03c8c77c
chron1kle <saundershwu@gmail.com>
2024-04-28 19:22:37 +0800

    S

--- dogbot_server/dogbot_server/dogbot_server.py
+++ dogbot_server/dogbot_server/dogbot_server.py
@@ -11,7 +11,7 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
-DEFAULT_LINEAR_VELOCITY = 0.8
+DEFAULT_LINEAR_VELOCITY = 0.4
 DEFAULT_ANGULAR_VELOCITY = -1.4
 
 
